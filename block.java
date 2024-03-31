b0e4c512a298845641f7b5316da8bc50c92eec41
prit1310 <pritsenjaliya1162@gmail.com>
2024-03-31 11:09:47 +0530

    add block code

---
+++ block.java
@@ -0,0 +1,11 @@
+public class Block {
+static {
+System.out.println("Static Block-1");
+}
+public static void main(String args[]) {
+System.out.println("Main Method");
+}
+static {
+System.out.println("Static Block-2");
+}
+}
