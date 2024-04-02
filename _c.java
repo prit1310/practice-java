7d4dc55380cb2c8a6dd4d4283f98201b96e7ec57
prit1310 <pritsenjaliya1162@gmail.com>
2024-04-02 23:29:20 +0530

    show output code

---
+++ _c.java
@@ -0,0 +1,10 @@
+public class _C
+{
+    private static int $;
+    public static void main(String main[]) 
+    {
+        String a_b="prit";
+        System.out.print($);
+        System.out.print(a_b);
+    }
+}
