ef5824137cf31beda769111a6244991d066258ec
prit1310 <pritsenjaliya1162@gmail.com>
2024-04-03 23:04:11 +0530

    get output of this code

---
+++ equals.java
@@ -0,0 +1,10 @@
+class equals 
+{
+    public static void main(String [] args) 
+    {
+        int x = 100;
+        double y = 100.1;
+        boolean b = (x = y); /* Line 5 */
+        System.out.println(b);
+    }
+}
