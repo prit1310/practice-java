2510d22dc7ba39b2e322b17d3c480fc92dfb6b13
prit1310 <pritsenjaliya1162@gmail.com>
2024-04-07 23:43:19 +0530

    add fab series

---
+++ fab.java
@@ -0,0 +1,16 @@
+class  fab
+{
+	public static void main(String args[])
+	{
+		int a=0,b=1;
+		System.out.println(a+"\n"+b);
+		int c;
+		for(int i=1;i<=10;i++)
+		{
+			c=a+b;
+			System.out.println(""+c);
+			a=b;
+			b=c;
+		}
+	}
+}
