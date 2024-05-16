71a1c79158389a88b11d7a012e1945844c786493
prit1310 <pritsenjaliya1162@gmail.com>
2024-05-16 16:51:38 +0530

    add code

---
+++ rotateArray.java
@@ -0,0 +1,36 @@
+public class rotateArray {
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;  
+        if (k == 0) {
+            return ; 
+        }
+
+        reverse(nums, 0, n - 1);      
+        reverse(nums, 0, k - 1);      
+        reverse(nums, k, n - 1);      
+    }
+
+    static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotate(nums, k);
+
+        System.out.println("Array after rotating by " + k + " steps:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
